c62867b428ee532500c6e2a99d689a7359660183
Dks-believer <mail2dpk.india@gmail.com>
2023-09-30 00:14:57 +0530

    dnkd

---
+++ math.js
@@ -0,0 +1,18 @@
+
+// function add (a,b){
+//  return a+b ;
+// }
+
+// function sub(a,b){
+
+//     return a-b;
+// }
+// //  module.exports = {add , sub} //we are sending the functions by binding inside the object
+// module.exports = {
+//     addfunction:add,
+//     subFuntion:sub,
+// }
+
+
+exports.add1 =(a,b) => a+b; // anonymous function, here add is not a function only we are refering a name , we can provide it any name
+exports.sub1 =(a,b) => a-b;
---
+++ server.js
@@ -0,0 +1,19 @@
+
+//  const {add , sub, addfunction, subFuntion } = require("./math");
+
+// console.log("addition is " ,addfunction(2,4));
+// console.log("substraction is" , subFuntion(6,2));
+
+
+const  math = require("./math");
+// console.log("addition is " ,math.add(2,4));
+// console.log("substraction is" , math.sub(6,2));
+
+// console.log(math); 
+// //output={ addfunction: [Function: add], subFuntion: [Function: sub] }
+
+
+// const {add1, sub1} = require("./math");
+
+console.log("addition is " ,math.add1(2,4));
+console.log("substraction is" , math.sub1(6,2));
